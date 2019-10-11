fe06c7c10258c2ba0f22834569d4c47fc95a6bad
David Dubois <dexcelstraun7@gmail.com>
2019-10-10 21:07:44 -0700

    I think I literally just wrote the interpreter

--- apps/bf/bf.main.cpp
+++ apps/bf/bf.main.cpp
@@ -3,8 +3,77 @@
 //! This file provides a command-line interface to the Brainfuck interpreter
 
 #include <iostream>
+#include <fstream>
+#include <algorithm>
 
 #include "error_codes.hpp"
+
+constexpr auto MIN_TAPE_SIZE = 32768u;
+
+/*!
+ * \brief Interprets a stream of Brainfuck instructions
+ *
+ * This interpreting routine has a minimum tape size of 32768 bytes. Each byte is 8 bits
+ *
+ * \param token_stream The stream of Brainfuck instructions to interpret
+ * \param tape_size The size of the tape that Brainfuck program will use. If this size is less than 32768, your program will be given a 32768-byte long tape
+ * \param bstdin Standard input stream that the program will read from
+ * \param bstdout Standard output stream that the program will write to
+ */
+bool interpret(std::istream& token_stream, const uint32_t& tape_size, std::istream& bstdin, std::ostream& bstdout) {
+	const uint32_t real_tape_size = std::min(tape_size, MIN_TAPE_SIZE);
+	const auto tape = std::make_unique<char*>(new char[real_tape_size]);
+	
+	auto* tape_ptr = *tape;
+	auto tape_idx = 0;
+	auto loop_start = token_stream.tellg();
+
+	while (token_stream.good()) {
+		const char token = token_stream.get();
+
+		switch (token) {
+		case '<':
+			tape_idx = (tape_idx + 1) % MIN_TAPE_SIZE;
+			break;
+
+		case '>':
+			tape_idx = (tape_idx - 1) % MIN_TAPE_SIZE;
+			break;
+
+		case '+':
+			++tape_ptr[tape_idx];
+			break;
+
+		case '-':
+			--tape_ptr[tape_idx];
+			break;
+
+		case '.':
+			bstdout << tape_ptr[tape_idx];
+			break;
+
+		case ',':
+			bstdin >> tape_ptr[tape_idx];
+			break;
+
+		case '[':
+			loop_start = token_stream.tellg();
+			break;
+
+		case ']':
+			if (tape_ptr[tape_idx]) {
+				token_stream.seekg(loop_start);
+			}
+			break;
+
+		default:
+			std::cerr << "Invalid token `" << token << "`\n";
+			return false;
+		}
+	}
+
+	return true;
+}
 
 /*!
  * \brief Entry point for my Brainfuck interpreter
@@ -17,16 +86,24 @@
  */
 int main(const char** argc, const int argv) {
 	try {
-		if(argv != 1) {
+		if (argv != 1) {
 			std::cerr << "bf only accepts a single argument: the name of the file to interpret\n";
 			return BF_ERR_TOO_MANY_ARGUMENTS;
 		}
 
-		std::cout << "Hello World!\n";
+		std::ifstream file_stream(argc[0]);
+
+		const auto valid_program = interpret(file_stream, 0, std::cin, std::cout);
+		if (!valid_program) {
+			std::cout << "Program " << argc[0] << " is invalid";
+
+			return BF_ERR_INVALID_PROGRAM;
+		}
 
 		return 0;
 
-	} catch(...) {
+	}
+	catch (...) {
 		return BF_ERR_UNKNOWN;
 	}
 }
--- apps/bf/error_codes.hpp
+++ apps/bf/error_codes.hpp
@@ -2,5 +2,6 @@
 
 //! Error codes that bf might return
 
-constexpr uint32_t BF_ERR_UNKNOWN = -0x01;
-constexpr uint32_t BF_ERR_TOO_MANY_ARGUMENTS = -0x10;
+constexpr auto BF_ERR_UNKNOWN = -0x01;
+constexpr auto BF_ERR_TOO_MANY_ARGUMENTS = -0x10;
+constexpr auto BF_ERR_INVALID_PROGRAM = -0x11;
