722b1ff7541a2af9daa7711bb6c2ba02b371e3a4
David Dubois <dexcelstraun7@gmail.com>
2019-10-10 20:21:53 -0700

    [ide] Add ReSharper settings

--- apps/bf/bf.main.cpp
+++ apps/bf/bf.main.cpp
@@ -1,11 +1,31 @@
 //! \brief Brainfuck interpreter
 //!
-//! This file provides a command-line interface to the brainfuck interpreter
+//! This file provides a command-line interface to the Brainfuck interpreter
 
 #include "iostream"
 
+/*!
+ * \brief Entry point for my Brainfuck interpreter
+ *
+ * Usage: `bf <filename>`
+ *
+ * Parameters:
+ * - filename: Name of the brainfuck file to interpret. May have any extension of reside on any drive accessible
+ * through the current filesystem
+ */
 int main(const char** argc, const int argv) {
-    std::cout << "Hello World!\n";
+	try {
+		if(argv != 0)
+		{
+			
+		}
+		
+		std::cout << "Hello World!\n";
 
-    return 0;
+		return 0;
+
+	}
+	catch (...) {
+		return 1;
+	}
 }
